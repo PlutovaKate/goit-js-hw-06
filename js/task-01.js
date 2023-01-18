cebd1312c71205005411993167a7719f38dea2d1
PlutovaKate <114903556+PlutovaKate@users.noreply.github.com>
2023-01-18 19:14:27 +0300

    добавила по первой задаче 2й вариант

--- js/task-01.js
+++ js/task-01.js
@@ -1,13 +1,27 @@
-"user strict";
+"use strict";
 
-const itemEl = document.querySelectorAll(`.item`);
-console.log(`Number of categories:`, itemEl.length);
+const itemsEl = document.querySelectorAll(".item");
+console.log(`Number of categories: ${itemsEl.length}`);
 
-const itemElArray = [...itemEl]
-  .map(
-    (item) => `Category: ${item.children[0].textContent}
-        Elements:${item.children[1].children.length}`
-  )
-  .join("\n");
+for (let index = 0; index < itemsEl.length; index++) {
+  const categoryEl = itemsEl[index].firstElementChild.innerText;
+  const liElement = itemsEl[index].children[1].children.length;
 
-console.log(itemElArray);
+  console.log(`Category: ${categoryEl}`);
+  console.log(`Elements: ${liElement}`);
+}
+
+// 2-ий варіант
+
+// const itemsEl = document.querySelectorAll(".item");
+// console.log(`Number of categories: ${itemsEl.length}`);
+
+// const itemsElArray = [...itemsEl]
+//   .map(
+//     (item) => `Category: ${item.children[0].textContent}
+// Elements: ${item.children[1].children.length}`
+//   )
+//   .join("\n");
+
+// console.log(itemsElArray);
+
